3cc6d07e168a68a9264a79378aef715e0545dea7
ailin torre val <aili0057@stud.kea.dk>
2022-03-20 12:05:14 +0100

    needs animation, fix menu, and add illustations

---
+++ assignment-portfolio.js
@@ -0,0 +1,15 @@
+"use strict";
+
+document.querySelector(".nav-menu").addEventListener("click", showMenu);
+
+function showMenu() {
+  document.querySelector(".collapsable-menu").classList.remove("hidden");
+  console.log("button open clicked");
+  document.querySelector(".nav-menu").addEventListener("click", closeMenu);
+}
+
+function closeMenu() {
+  document.querySelector(".collapsable-menu").classList.add("hidden");
+  console.log("button close clicked");
+  document.querySelector(".nav-menu").addEventListener("click", showMenu);
+}
