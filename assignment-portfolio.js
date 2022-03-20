3c0e854c9eb672f011d9d758f9c6e396a3fc2122
ailin torre val <aili0057@stud.kea.dk>
2022-03-20 20:45:48 +0100

    some touch ups, need animations

--- assignment-portfolio.js
+++ assignment-portfolio.js
@@ -3,6 +3,7 @@
 document.querySelector(".nav-menu").addEventListener("click", showMenu);
 
 function showMenu() {
+    document.querySelector(".nav-menu").removeEventListener("click", showMenu);
   document.querySelector(".collapsable-menu").classList.remove("hidden");
   console.log("button open clicked");
   document.querySelector(".nav-menu").addEventListener("click", closeMenu);
