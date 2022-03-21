2b78a2d0fe454004454b657f8669a2f49d2bd86e
ailin torre val <aili0057@stud.kea.dk>
2022-03-21 01:15:47 +0100

    finished

--- assignment-portfolio.js
+++ assignment-portfolio.js
@@ -1,9 +1,25 @@
 "use strict";
+gsap.registerPlugin(ScrollTrigger);
 
 document.querySelector(".nav-menu").addEventListener("click", showMenu);
 
+gsap.to("#intro-pic", {
+  duration: 3,
+  // x: "45vw",
+  scale: 4,
+  scrollTrigger: {
+    start: "top 75%",
+    trigger: "#intro-pic",
+    end: "bottom 95%",
+    toggleActions: "play pause resume reset",
+    scrub: true,
+  },
+});
+
 function showMenu() {
-    document.querySelector(".nav-menu").removeEventListener("click", showMenu);
+  document.querySelector(".nav-menu").removeEventListener("click", showMenu);
+  document.querySelector(".arrow-right").classList.add("hidden");
+  document.querySelector(".arrow-down").classList.remove("hidden");
   document.querySelector(".collapsable-menu").classList.remove("hidden");
   console.log("button open clicked");
   document.querySelector(".nav-menu").addEventListener("click", closeMenu);
@@ -11,6 +27,8 @@ function showMenu() {
 
 function closeMenu() {
   document.querySelector(".collapsable-menu").classList.add("hidden");
+  document.querySelector(".arrow-down").classList.add("hidden");
+  document.querySelector(".arrow-right").classList.remove("hidden");
   console.log("button close clicked");
   document.querySelector(".nav-menu").addEventListener("click", showMenu);
 }
